0d4f3f4497213dab65a03bc50951bb52dbd252c3
arathikrishnaam <krishnaamarathi@gmail.com>
2026-02-18 05:26:20 +0000

    trigger

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("Hello from GitOps v2!")
+    print("Hello from GitOps v2! This is a simple Flask application.")
     return jsonify({
         "message": "Hello from GitOps version 2! This is a simple Flask application.",
         "version": os.getenv("APP_VERSION", "1.0.0")
